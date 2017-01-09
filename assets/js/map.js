e54dc05340a3404c524f3b825ac8ffb3b39fd004
William Walcher <wjwalcher@gmail.com>
2017-01-09 16:03:39 -0600

    Initial Commit

---
+++ assets/js/map.js
@@ -0,0 +1,41 @@
+window.onerror = function(error) {
+    alert(error);
+};
+
+  function initialize() {
+        var mapOptions = {
+          zoom: 16,
+          draggable: false,
+          scrollwheel:false,
+          navigationControl: false,
+          mapTypeControl: false,
+          scaleControl: false,
+          center: new google.maps.LatLng(44.902764, -93.563524),
+          mapTypeId: google.maps.MapTypeId.ROAD
+        };
+        map = new google.maps.Map(document.getElementById('map'),
+            mapOptions);
+        new google.maps.Marker({map:map,position:map.getCenter(),label:"Excelsior Commons"})
+        
+        
+
+  google.maps.event.addListener(map, 'center_changed', function() {
+    
+    //a value to determine whether the map has been resized
+    var size=[this.getDiv().offsetWidth,this.getDiv().offsetHeight].join('x');
+    
+    //when the center has changed, but not the size of the map
+    if(!this.get('size') || size===this.get('size')){
+       console.log('ccc');
+       this.setValues({size:size,_center:this.getCenter()});   
+       google.maps.event.trigger(map, "resize");      
+    }
+    //when the map has been resized
+    else{
+      google.maps.event.addListenerOnce(this,'idle',function(){console.log('rrr');
+      this.setValues({size:size,center:this.get('_center')});});      
+    }
+  });
+  //trigger the resize-event to initialize the size and _center-values
+  google.maps.event.trigger(map,'center_changed',{});
+}
